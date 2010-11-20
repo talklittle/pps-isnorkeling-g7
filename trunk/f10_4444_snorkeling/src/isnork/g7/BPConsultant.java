12b9b501141e12302344ae34001c5c2e0e54cdd4
talklittle <talklittle@4dd3b2dc-95fc-47e2-c4d5-241bbd00fc48>
2010-11-20 03:50:05 +0000

    g7.BPConsultant. WIP trying to get NavigateToBoat working

---
+++ trunk/f10_4444_snorkeling/src/isnork/g7/BPConsultant.java
@@ -0,0 +1,110 @@
+package isnork.g7;
+
+
+import isnork.sim.GameConfig;
+import isnork.sim.Observation;
+import isnork.sim.Player;
+import isnork.sim.SeaLifePrototype;
+import isnork.sim.iSnorkMessage;
+import isnork.sim.GameObject.Direction;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+
+
+public class BPConsultant extends Player {
+
+	// A buffer to give you some extra time to go back to boat
+	private static final int BOAT_TIME_BUFFER = 5;
+	
+	private static final Logger logger = Logger.getLogger(BPConsultant.class);
+	
+	private Direction direction;
+	Point2D whereIAm = null;
+	
+	Set<SeaLifePrototype> seaLifePossibilities;
+	int d = -1;  // parameter "d" == half the board length
+	int r = -1;  // parameter "r" == number of cells away you can see
+	int n = -1;  // parameter "n" == number of snorkelers
+	int penalty;
+	int round = 0;
+	
+	@Override
+	public String getName() {
+		return "BP Consultant";
+	}
+	
+	@Override
+	public String tick(Point2D myPosition, Set<Observation> whatYouSee,
+			Set<iSnorkMessage> incomingMessages,Set<Observation> playerLocations) {
+		String snorkMessage = null;
+		whereIAm = myPosition;
+		if(n % 10 == 0)
+			snorkMessage = "s";
+		else
+			snorkMessage = null;
+		
+		round++;
+		return snorkMessage;
+	}
+	
+	@Override
+	public Direction getMove() {
+		
+		// Head back to boat if we are running low on time (shortest time back, plus a buffer)
+		if (getRemainingTime() <= NavigateToBoat.getTimeToBoat(whereIAm) + BOAT_TIME_BUFFER) {
+			logger.debug("time remaining: " + getRemainingTime() + " heading back to boat");
+			return NavigateToBoat.getShortestDirectionToBoat(whereIAm);
+		} else {
+			Direction d = getNewDirection();
+			
+			Point2D p = new Point2D.Double(whereIAm.getX() + d.dx,
+					whereIAm.getY() + d.dy);
+			while (Math.abs(p.getX()) > GameConfig.d
+					|| Math.abs(p.getY()) > GameConfig.d) {
+				d = getNewDirection();
+				p = new Point2D.Double(whereIAm.getX() + d.dx,
+						whereIAm.getY() + d.dy);
+			}
+			return d;
+		}
+	}
+	
+	@Override
+	public void newGame(Set<SeaLifePrototype> seaLifePossibilities, int penalty,
+			int d, int r, int n) {
+		this.seaLifePossibilities = seaLifePossibilities;
+		this.penalty = penalty;
+		this.d = d;
+		this.r = r;
+		this.n = n;
+		this.round = 0;
+	}
+
+
+	private Direction getNewDirection() {
+		int r = random.nextInt(100);
+		if(r < 10 || direction == null)
+		{
+			ArrayList<Direction> directions = Direction.allBut(direction);
+			direction = directions.get(random.nextInt(directions.size()));
+		}
+			return direction;
+	}
+	
+	/**
+	 * Get the remaining time out of 480 rounds (8 hours).
+	 * The final round will return 0.
+	 * GameEngine starts with round 0, and Game Over is before beginning of round 481.
+	 * @return
+	 */
+	private int getRemainingTime() {
+		return 480 - round;
+	}
+
+
+}
