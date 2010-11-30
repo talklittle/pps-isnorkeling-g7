11c43631787f62cf5ac3cb4c0cb1043698d3f266
iterdan@gmail.com <iterdan@gmail.com@4dd3b2dc-95fc-47e2-c4d5-241bbd00fc48>
2010-11-30 00:55:00 +0000

    added message translator and tracker.

---
+++ f10_4444_snorkeling/src/isnork/g7/Tracker.java
@@ -0,0 +1,30 @@
+package isnork.g7;
+
+import java.awt.geom.Point2D;
+
+import org.apache.log4j.Logger;
+
+import isnork.sim.SeaLifePrototype;
+import isnork.sim.GameObject.Direction;
+
+public class Tracker {
+
+	private static final Logger logger = Logger.getLogger(Tracker.class);
+	
+	public static Direction track(SeaLifePrototype s, Point2D me, Point2D beast)
+	{
+		return tightTrack(me, beast);
+	}
+
+	private static Direction safeTrack(Point2D me, Point2D beast) {
+		return null;
+	}
+
+	private static Direction tightTrack(Point2D me, Point2D beast) 
+	{
+		if(beast == null)
+			return null;
+		return OurBoard.getDirectionTowards(me, beast);
+	}
+	
+}
