6045cb5d10af4c852ff84bf053153986e8dd8493
elizabeth.kierstead <elizabeth.kierstead@4dd3b2dc-95fc-47e2-c4d5-241bbd00fc48>
2010-11-30 03:21:29 +0000

    Added addTask() and getTask() functionality to TaskManager.

--- f10_4444_snorkeling/src/isnork/g7/TaskManager.java
+++ f10_4444_snorkeling/src/isnork/g7/TaskManager.java
@@ -1,8 +1,12 @@
 package isnork.g7;
 
+import isnork.sim.SeaLifePrototype;
+
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class TaskManager {
 	private PriorityQueue<Task> taskList; 
@@ -10,27 +14,34 @@ public class TaskManager {
 	//Hash of the SeaLife creature name Strings mapping to the number of times it was seen
 	//by the diver using this class
 	private HashMap<String, Integer> seenCreatures;
+	private OurBoard ourBoard;
+	private Set<SeaLifePrototype> seaLifePossibilities;
 	
-	public TaskManager(){
+	public TaskManager(Set<SeaLifePrototype> seaLifePossibilities, OurBoard ourBoard){
 		taskList = new PriorityQueue<Task>(10, new TaskComparator());
 		seenObjects = new HashMap<Task, Boolean>();
+		this.seaLifePossibilities = seaLifePossibilities;
+		this.ourBoard = ourBoard;
 	}
 
 	
 	public static class TaskComparator implements Comparator<Task>
     {
-        public int compare (Task o1, Task o2)
+        @SuppressWarnings("unchecked")
+		public int compare (Task o1, Task o2)
         {
-            return ((Comparable) o1).compareTo(o2);
+            return ((Comparable<Task>) o1).compareTo(o2);
         }
     }
 	
 	//TO-DO: Finish this implementation
-	public void addTask(){
-		//Task task = new Task()
-		//taskList.add(task);
-		//seenObjects.put(task, false);
-		//seenCreatures.put(creatureName, new Integer(numCreatureSightings++));
+	public void addTask(String creatureName, int playerID){
+		Task task = new Task(creatureName, playerID, ourBoard);
+		taskList.add(task);
+		seenObjects.put(task, false);
+		
+		int numPreviousSightings = seenCreatures.get(creatureName).intValue();
+		seenCreatures.put(creatureName, new Integer(numPreviousSightings++));
 
 	}
 	
@@ -39,13 +50,15 @@ public class TaskManager {
 	public Task getNextTask(){
 		Task nextTask = taskList.remove();
 		
-		
 		while (!nextTask.getObservation().isValid() || !taskUnseen(nextTask)){
 			nextTask = taskList.remove();
 		}
 		
-		return nextTask;
+		if(nextTask != null){
+			markTaskComplete(nextTask);
+		}
 		
+		return nextTask;
 	}
 	
 	private boolean taskUnseen(Task task){
@@ -62,10 +75,25 @@ public class TaskManager {
 	public void markTaskComplete(Task task){
 		seenObjects.put(task, true);
 		task.getObservation().setInvalid();
+		markCreatureSeen(task.getObservation().getCreatureName());
 	}
 	
-	public void markCreatureSeen(String creatureName){
+	private void markCreatureSeen(String creatureName){
 		int numCreatureSightings = seenCreatures.get(creatureName).intValue();
 		seenCreatures.put(creatureName, new Integer(numCreatureSightings++));
+	}
+	
+	private SeaLifePrototype getCreature(String creatureName){
+		Iterator<SeaLifePrototype> seaLifeIt = seaLifePossibilities.iterator();
+		
+		SeaLifePrototype nextCreature = seaLifeIt.next();
+		
+		while(nextCreature!=null){
+			if (nextCreature.getName() == creatureName) {
+				return nextCreature;
+			}
+		}
+		
+		return nextCreature;
 	}
 }
